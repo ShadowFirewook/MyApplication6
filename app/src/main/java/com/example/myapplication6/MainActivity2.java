4e1695361da5d2604bbb6f750b25dcf5ccf2eccc
ShadowFirewook <104684219+ShadowFirewook@users.noreply.github.com>
2022-09-11 19:49:04 +0600

    Update MainActivity2.java

--- app/src/main/java/com/example/myapplication6/MainActivity2.java
+++ app/src/main/java/com/example/myapplication6/MainActivity2.java
@@ -8,12 +8,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
-
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("text");
+        textView = findViewById(R.id.text);
+        textView.setText(text);
     }
 }
